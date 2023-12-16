2b058e98d3b7dc4f0be5b47140a81effde882378
Rodrigo Jose Fagundes <rodrigojosefagundes@gmail.com>
2023-12-16 18:43:11 -0300

    Navigating pages with router links

--- frontend/carwork-app/src/app/client/client-routing.module.ts
+++ frontend/carwork-app/src/app/client/client-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ClientFormComponent } from './client-form/client-form.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+{ path: 'client-form', component: ClientFormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
