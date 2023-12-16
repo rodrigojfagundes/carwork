b40b8e8908292f96b47cc400ab539f5681e7eb33
Rodrigo Jose Fagundes <rodrigojosefagundes@gmail.com>
2023-12-16 19:07:54 -0300

    Created AuthGuard

--- frontend/carwork-app/src/app/app.module.ts
+++ frontend/carwork-app/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,16 +12,23 @@ import { ClientModule } from './client/client.module';
 import { ClientService } from './client.service';
 import { CarModule } from './car/car.module';
 import { CarService } from './car.service';
-import { WorkModule } from './work/work.module';  
+import { WorkModule } from './work/work.module';
 import { WorkService } from './work.service';
+import { LoginComponent } from './login/login.component.ts';
+import { LayoutComponent } from './layout/layout.component.ts';
+import { AuthService } from './auth.service';
+import { TokenInterceptor } from './token.interceptor';
 
 @NgModule({
     declarations: [
         AppComponent,
-        HomeComponent
+        HomeComponent,
+        LoginComponent,
+        LayoutComponent
     ],
     imports: [
         BrowserModule,
+        FormsModule,
         HttpClientModule,
         AppRoutingModule,
         TemplateModule,
@@ -31,7 +39,13 @@ import { WorkService } from './work.service';
     providers: [
         ClientService,
         CarService,
-        WorkService
+        WorkService,
+        AuthService,
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: TokenInterceptor,
+            multi: true
+        }
     ],
     bootstrap: [AppComponent]
 })
