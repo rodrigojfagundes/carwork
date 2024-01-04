cdb9603a9d3e1a9179e8ff005cf4d70dcd2803ec
Rodrigo Jose Fagundes <rodrigojosefagundes@gmail.com>
2024-01-03 22:41:30 -0300

    Update layout.component.ts

--- frontend/carwork-app/src/app/layout/layout.component.ts
+++ frontend/carwork-app/src/app/layout/layout.component.ts
@@ -1,32 +1,32 @@
-import { Component, AfterViewInit } from '@angular/core';
-
-import jQuery from 'jquery'
+import { Component, AfterViewInit } from "@angular/core";
+import JQuery from 'jquery'
 
 @Component({
-  selector: 'app-layout',
-  templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.css']
+    selector: 'app-layout',
+    templateUrl: './layout.component.html',
+    styleUrls: ['./layout.component.css']
 })
 export class LayoutComponent implements AfterViewInit {
-//  title = 'carwork-app';
 
-constructor() {}
+    constructor() { }
 
-  ngAfterViewInit(){
-    (function($) {
-        "use strict";
-    
-        // Add active state to sidbar nav links
-        var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
-            $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function() {
+    ngAfterViewInit() {
+        (function ($) {
+            "use strict";
+
+            var path = window.location.href;
+            $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function () {
                 if (this.href === path) {
                     $(this).addClass("active");
                 }
             });
-    
-    })(jQuery);
-  }
+
+            // Toggle the side navigation
+            $("#sidebarToggle").on("click", function (e) {
+                e.preventDefault();
+                $("body").toggleClass("sb-sidenav-toggled");
+            });
+        })(JQuery);
+    }
+
 }
-
-
-
